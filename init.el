56a6f32a4861f4f48ed3b2ae52951855d521c338
Duff OMelia <duff@omelia.org>
2015-06-17 18:49:24 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,7 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
