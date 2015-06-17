d93f0fb6579cc89f58a35ebe9906eb6cc3eb40a9
Duff OMelia <duff@omelia.org>
2015-06-17 18:57:01 -0400

    Use evil of course

--- init.el
+++ init.el
@@ -5,3 +5,16 @@
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
+
+
+;; Refresh the package descriptions
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; No backup files.  No menubar.  No toolbar.
+(setq make-backup-files nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(require 'evil)
+(evil-mode 1)
