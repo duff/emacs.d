d52f195a48e6161208f555255d85478e2ebe344c
Duff OMelia <duff@omelia.org>
2015-06-17 22:23:25 -0400

    Hook up more goodies

--- init.el
+++ init.el
@@ -18,7 +18,30 @@
 (tool-bar-mode -1)
 
 (setq inhibit-startup-screen t)
+(setq ns-use-native-fullscreen nil)
+
+(add-to-list 'default-frame-alist '(height . 58))
+(add-to-list 'default-frame-alist '(width . 178))
+
+(global-set-key (kbd "<s-return>") 'toggle-frame-fullscreen)
+
+;; Save histories
+(setq savehist-additional-variables '(search-ring regexp-search-ring))
+(savehist-mode 1)
 
 (require 'evil)
 (evil-mode 1)
+
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
+(evil-leader/set-key
+  "w"  'save-buffer
+  "v" 'split-window-right
+  "s" 'split-window-below
+  )
+
+(require 'key-chord)(key-chord-mode 1)
+(key-chord-define evil-insert-state-map ";;" 'evil-normal-state)
+(key-chord-define evil-visual-state-map ";;" 'evil-change-to-previous-state)
 
