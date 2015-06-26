4a27c9b76a9f91d776864042ce394558ed37020f
Duff OMelia <duff@omelia.org>
2015-06-26 06:21:57 -0400

    Lessen emphasis on buffer id in modeline

--- customize.el
+++ customize.el
@@ -15,4 +15,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "alternateSelectedControlColor")))))
+ '(cursor ((t (:background "alternateSelectedControlColor"))))
+ '(mode-line-buffer-id ((t (:foreground "selectedKnobColor")))))
