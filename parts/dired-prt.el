5bf3c56c0eab7baefb2719034834925fe1dc3267
Duff O'Melia <duff@omelia.org>
2016-01-11 16:00:18 -0500

    Show directories first

--- parts/dired-prt.el
+++ parts/dired-prt.el
@@ -3,5 +3,10 @@
 ;; Auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 
+(require 'ls-lisp)
+(setq ls-lisp-use-insert-directory-program t)
+(setq insert-directory-program "/usr/local/Cellar/coreutils/8.24/libexec/gnubin/ls")
+
+(setq dired-listing-switches "--group-directories-first -alh")
 
 (provide 'dired-prt)
