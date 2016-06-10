2c78d10159a6fc1d8215d62697a7dfa20b56fd54
Duff O'Melia <duff@omelia.org>
2016-06-10 16:41:44 -0400

    Fix ctags

--- parts/project-prt.el
+++ parts/project-prt.el
@@ -2,7 +2,10 @@
 (persp-mode)
 
 (setq projectile-enable-caching t)             ;; Cache the things
-(setq projectile-idle-timer-seconds 60)        ;; Regnerate tags when idle
+
+;; Regnerate tags when idle - defaults to 30 seconds
+(setq projectile-enable-idle-timer t)
+
 (setq projectile-sort-order (quote recentf))
 
 (setq projectile-project-root-files
